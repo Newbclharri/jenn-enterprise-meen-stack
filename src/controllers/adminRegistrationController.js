27253826ff4be5ca65663a08d7e5a634423098ca
contactharrisc2 <contactharrisc2@gmail.com>
2024-08-05 21:21:35 -0500

    Corrects the access to the URL token (should be req.query.token as indicated in the get URL)

--- src/controllers/adminRegistrationController.js
+++ src/controllers/adminRegistrationController.js
@@ -9,8 +9,21 @@ const jwToken = require('jsonwebtoken');
 
 //////////////// MIDDLEWARE ////////////////
 
+/**
+ * Middleware function to verify the admin invite token from the request query.
+ * If the token is missing, invalid, or expired, a 401 Unauthorized response is sent.
+ * 
+ * @function verifyAdminInviteToken
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function to call if the token is valid.
+ * @returns {Object} - Sends a response to the client, either a 401 status or calls the next middleware.
+ * 
+ * @throws {Error} Throws an error if the token verification fails.
+ */
+
 exports.verifyAdminInviteToken = function (req, res, next) {
-    const adminInviteToken = req.query.adminInviteToken;
+    const adminInviteToken = req.query.token;
 
     if (!adminInviteToken) {
         return res.status(401).send('Not Authorized to access this resource');
